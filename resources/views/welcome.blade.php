12174767319791ff958cc673b826f8cf8d45a844
“Frank <frank@frankjones.co>
2023-03-18 09:42:27 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Web designer based in Tavira Algarve</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+    <i class="fa-solid fa-trash"></i>
+    Hello, My name is
+
+    Frank Jones
+
+    I am a web developer and brand designer
+
+    From London, England, I have rich experience if producing a wide variety of web applications and sites.
+
+    Download CV
+
+    My Skills
+
+
+    15 Years Experiene 
+
+
+    330 completed projects
+    
+</body>
+</html>
